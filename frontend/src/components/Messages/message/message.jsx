7b3bca1e25568830ca07d05d182d0d5c14515ac1
Gian_Pereira <gian.21.otaku@gmail.com>
2024-10-25 13:18:54 -0300

    Preparing some modularization of messages

--- frontend/src/components/Messages/message/message.jsx
+++ frontend/src/components/Messages/message/message.jsx
@@ -3,34 +3,61 @@ import user_profile from './user-profile.png';
 
 import './message.css'
 
+
+const PROFILE_PHOTO = jason_profile
+
+
 export function Message({ data }) {
-let is_user = data.user
-let message_styles = 'message_styles message_styles'
-let profile_photo = user_profile
-if (is_user) {
-    message_styles = 'user_'+message_styles
-} else {
-    message_styles = 'robot_'+message_styles
-    profile_photo = jason_profile
-}
-let current_time = new Date();
-return (
-    <div className={message_styles}>
-    <div className={message_styles+'--image_container'}>
-        <img className={message_styles+'--image'} src={profile_photo} alt="Jasonbot"/>
-    </div>
-    <div className={message_styles+'--text_container'}>
-        <div className={message_styles+'--header'}>
-        <legend>{data.subject}</legend>
-        <time>{current_time.getDate()}/{current_time.getMonth()}/{current_time.getFullYear()} - {current_time.getHours()}:{current_time.getMinutes()}</time>
+    let is_user = data.user
+    let current_time = new Date();
+
+    let message_styles = verify_subject(is_user)
+
+    return (
+        <div className={message_styles}>
+        <div className={message_styles+'--image_container'}>
+            <img className={message_styles+'--image'} src={PROFILE_PHOTO} alt="Jasonbot"/>
         </div>
-        <div>
-        <p>{data.text}</p>
-        <p>{data.title}</p>
-        <p>{data.lang}</p>
+        <div className={message_styles+'--text_container'}>
+            <div className={message_styles+'--header'}>
+                <legend>{data.subject}</legend>
+                <time>{current_time.getDate()}/{current_time.getMonth()}/{current_time.getFullYear()} - {current_time.getHours()}:{current_time.getMinutes()}</time>
+            </div>
+            <div>
+                {build_message_body(data)}
+            </div>
         </div>
-    </div>
-    </div>
-);
+        </div>
+    );
 }
+
+function AnalyseMessage ({data}) {
+    return (
+        <>
+            <h3>{data.text}</h3>
+            <hr/>
+            <p>{data.title}</p>
+            <p>{data.lang}</p>
+            <hr />
+        </>
+    )
+}
+
+
+function verify_subject (is_user) {
+    let message_styles = 'message_styles message_styles';
+    if (is_user) {
+        return message_styles = 'user_'+message_styles
+    } else {
+        return message_styles = 'robot_'+message_styles
+    }
+}
+
+function build_message_body (data) {
+    let messages_types = {
+        "analyse": <AnalyseMessage data={data}></AnalyseMessage>
+    }
+    return messages_types[data.request_chat]
+}
+
 
--- frontend/src/components/Messages/root.jsx
+++ frontend/src/components/Messages/root.jsx
@@ -3,8 +3,8 @@ import { useRef, useEffect } from "react";
 import './root.css';
 import { Message } from "./message/message";
 
-export default function MessageField({ messages }) {
 
+export default function MessageField({ messages }) {
     const messages_end_ref = useRef(null);
     
     useEffect(() => {
@@ -13,8 +13,9 @@ export default function MessageField({ messages }) {
     
     return (
         <div className='messages_field_container'>
-        {messages.map((message, index) => (
-            <Message key={index} data={message} />
+        {
+            messages.map((message, index) => (
+                <Message key={index} data={message} />
         ))}
         <div ref={messages_end_ref}></div>
         </div>
