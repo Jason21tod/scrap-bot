86974be9a82caa44b06d2042d7e7529e98c1ad50
Gian_Pereira <gian.21.otaku@gmail.com>
2024-10-30 13:08:53 -0300

    Making some Styles in new APP functions

--- frontend/src/components/Messages/message/analyse/analyse.jsx
+++ frontend/src/components/Messages/message/analyse/analyse.jsx
@@ -1,11 +1,13 @@
+import "./analyse.css"
+
 export default function AnalyseMessage ({data}) {
     return (
-        <>
-            <h3>{data.text}</h3>
-            <hr/>
-            <p>{data.title}</p>
-            <p>{data.lang}</p>
-            <hr />
-        </>
+        <div className="analyse_container">
+            <h3 className="analyse_container_text">{data.text}</h3>
+            <ul className="analyse_container_list">
+                <li className="analyse_container_list_item--title">{data.title}</li>
+                <li className="analyse_container_list_item--lang">{data.lang}</li>    
+            </ul>
+        </div>
     )
 }
